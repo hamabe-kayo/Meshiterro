fa5cb98c99d9a014f03f014a44b750595a44c83b
EC2 Default User <ec2-user@ip-172-31-93-14.ec2.internal>
2021-10-13 14:27:15 +0000

    [Add]詳細ページ作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b51db3fe6a321f3da8752891f01acb2ab0a6fd15821b401f6e3580aa12cc75f220ef40830bdbc79caa6e20c08ffe4789c01fcb2ef5831962701c7b7df2d5b8f5'
